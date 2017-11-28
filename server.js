fb73970d04cc8ddb18970e68ee95f3dfa2657595
janisnjohn <janisnjohn@gmail.com>
2017-11-28 16:26:28 -0700

    added mlab

--- server.js
+++ server.js
@@ -26,14 +26,14 @@ app.set('view engine', 'handlebars');
 // Database Configuration with Mongoose
 // ---------------------------------------------------------------------------------------------------------------
 // Connect to localhost if not a production environment
-// if(process.env.NODE_ENV == 'production'){
-//   mongoose.connect('mongodb://heroku_60zpcwg0:ubn0n27pi2856flqoedo9glvh8@ds119578.mlab.com:19578/heroku_60zpcwg0');
-// }
-// else{
+if(process.env.NODE_ENV == 'production'){
+mongoose.connect('MONGODB_URI: mongodb://heroku_m9bhzdg6:b50mg686apsl8lqhmkoaheh37u@ds123956.mlab.com:23956/heroku_m9bhzdg6');
+}
+else{
   mongoose.connect('mongodb://localhost/scrapingmongoose');
   // YOU CAN IGNORE THE CONNECTION URL BELOW (LINE 41) THAT WAS JUST FOR DELETING STUFF ON A RE-DEPLOYMENT
   //mongoose.connect('mongodb://heroku_60zpcwg0:ubn0n27pi2856flqoedo9glvh8@ds119578.mlab.com:19578/heroku_60zpcwg0');
-// }
+}
 var db = mongoose.connection;
 
 // Show any Mongoose errors
